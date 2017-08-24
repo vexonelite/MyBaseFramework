2a0aa69f0a529115aeadc6cee685db3845ad2acc
vexonelite <elite@realtime.tw>
2017-08-24 14:56:00 +0800

    add OptionDelegate (interface)

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/OptionDelegate.java
@@ -0,0 +1,10 @@
+package tw.realtime.project.rtbaseframework.interfaces;
+
+/**
+ * Created by vexonelite on 2017/8/23.
+ */
+
+public interface OptionDelegate {
+    String getTitle();
+    boolean isSelected(String title);
+}
