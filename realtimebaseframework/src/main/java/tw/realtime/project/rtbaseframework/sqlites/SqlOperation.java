4d7094d13db44ffdb5b798b8ded27aaa9810e4c6
vexonelite <elite@realtime.tw>
2017-11-21 17:36:55 +0800

    add 'CrossTableOperation' and 'SqlOperation'

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/sqlites/SqlOperation.java
@@ -0,0 +1,11 @@
+package tw.realtime.project.rtbaseframework.sqlites;
+
+/**
+ * Created by vexonelite on 2017/11/21.
+ */
+
+public enum  SqlOperation {
+    INSERT,
+    DELETE,
+    DELETE_WITH_CONDITION
+}
