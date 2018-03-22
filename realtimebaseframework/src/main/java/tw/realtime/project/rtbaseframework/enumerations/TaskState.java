98ed41a4e0bcb30c49560d5ecf2ddf0ce4fb258c
vexonelite <elite@realtime.tw>
2018-03-22 14:32:46 +0800

    add TaskState

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/enumerations/TaskState.java
@@ -0,0 +1,13 @@
+package tw.realtime.project.rtbaseframework.enumerations;
+
+/**
+ * Created by vexonelite on 2017/03/22.
+ */
+
+public enum TaskState {
+    DEFAULT,
+    LOADING_BEGIN,
+    LOADING_END,
+    SUCCESS,
+    FAILURE
+}
