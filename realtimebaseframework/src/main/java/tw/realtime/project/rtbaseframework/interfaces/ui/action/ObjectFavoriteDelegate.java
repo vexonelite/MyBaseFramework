5ca832a6cf2da13666ffe79e8200e6a722035a1d
vexonelite <elite@realtime.tw>
2018-05-30 11:43:14 +0800

    [Program][interfaces] add 3 to ui.action

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/ui/action/ObjectFavoriteDelegate.java
@@ -0,0 +1,9 @@
+package tw.realtime.project.rtbaseframework.interfaces.ui.action;
+
+/**
+ * Created by vexonelite on 2018/03/31.
+ */
+
+public interface ObjectFavoriteDelegate {
+    void onFavoriteChanged();
+}
---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/ui/action/ObjectShareDelegate.java
@@ -0,0 +1,9 @@
+package tw.realtime.project.rtbaseframework.interfaces.ui.action;
+
+/**
+ * Created by vexonelite on 2018/03/29.
+ */
+
+public interface ObjectShareDelegate {
+    void onDemandToShare();
+}
