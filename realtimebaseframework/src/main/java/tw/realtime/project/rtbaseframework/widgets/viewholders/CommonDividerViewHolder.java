2be9a7bf973bd3d3a69fbfc9b19abbeb4878f3d8
vexonelite <elite@realtime.tw>
2018-08-15 17:09:05 +0800

    add CommonDividerViewHolder and xml layout base_common_divider_viewholder

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/viewholders/CommonDividerViewHolder.java
@@ -0,0 +1,14 @@
+package tw.realtime.project.rtbaseframework.widgets.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by vexonelite on 2018/2/9.
+ */
+
+public final class CommonDividerViewHolder extends RecyclerView.ViewHolder {
+    public CommonDividerViewHolder(View itemView) {
+        super(itemView);
+    }
+}
---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/viewholders/LoadingViewHolder.java
@@ -0,0 +1,14 @@
+package tw.realtime.project.rtbaseframework.widgets.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by vexonelite on 2018/2/9.
+ */
+
+public final class LoadingViewHolder extends RecyclerView.ViewHolder {
+    public LoadingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
