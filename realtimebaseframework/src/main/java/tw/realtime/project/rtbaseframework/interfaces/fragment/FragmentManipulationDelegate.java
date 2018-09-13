347546418b0ec34ce1233fc830b5a84205520cf3
vexonelite <elite@realtime.tw>
2018-09-13 14:41:17 +0800

    interface/fragment: add FragmentManipulationDelegate and OnBackPressedDelegate

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/fragment/FragmentManipulationDelegate.java
@@ -0,0 +1,27 @@
+package tw.realtime.project.rtbaseframework.interfaces.fragment;
+
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by vexonelite on 2018/09/13.
+ */
+public interface FragmentManipulationDelegate {
+    /**
+     * Fragment 的取代或蓋頁
+     * @param targetFragment 要被顯示的 Fragment
+     * @param doesReplace   是否要取代或蓋頁
+     * @param containerResId 對應的容器 Id
+     */
+    void replaceOrShroudFragment (@NonNull Fragment targetFragment,
+                                  boolean doesReplace,
+                                  @IdRes final int containerResId);
+
+    void popAllFragmentsIfNeeded();
+
+    /**
+     * Pop 目前畫面中的 Fragment
+     */
+    void popFragmentIfNeeded();
+}
---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/fragment/OnBackPressedDelegate.java
@@ -0,0 +1,61 @@
+package tw.realtime.project.rtbaseframework.interfaces.fragment;
+
+/**
+ * @see <a href="https://tausiq.wordpress.com/2014/06/06/android-multiple-fragments-stack-in-each-viewpager-tab/">Android: Multiple Fragments stack in each ViewPager Tab</a>
+ * <p>
+ * Created by shahabuddin on 2014/06/06.
+ */
+public interface OnBackPressedDelegate {
+    boolean onBackPressed();
+
+    /*
+    @Override
+    public boolean onBackPressed() {
+        final int childCount = getChildFragmentManager().getBackStackEntryCount();
+        if (childCount == 0) {
+            // it has no child Fragment
+            // can not handle the onBackPressed task by itself
+            return false;
+
+        } else {
+            getChildFragmentManager().popBackStackImmediate();
+            // either this Fragment or its child handled the task
+            // either way we are successful and done here
+            return true;
+        }
+    }
+    */
+
+    /*
+    @Override
+    public boolean onBackPressed() {
+        if (parentFragment == null) return false;
+
+        int childCount = parentFragment.getChildFragmentManager().getBackStackEntryCount();
+
+        if (childCount == 0) {
+            // it has no child Fragment
+            // can not handle the onBackPressed task by itself
+            return false;
+
+        } else {
+            // get the child Fragment
+            FragmentManager childFragmentManager = parentFragment.getChildFragmentManager();
+            OnBackPressListener childFragment = (OnBackPressListener) childFragmentManager.getFragments().get(0);
+
+            // propagate onBackPressed method call to the child Fragment
+            if (!childFragment.onBackPressed()) {
+                // child Fragment was unable to handle the task
+                // It could happen when the child Fragment is last last leaf of a chain
+                // removing the child Fragment from stack
+                childFragmentManager.popBackStackImmediate();
+
+            }
+
+            // either this Fragment or its child handled the task
+            // either way we are successful and done here
+            return true;
+        }
+    }
+    */
+}
