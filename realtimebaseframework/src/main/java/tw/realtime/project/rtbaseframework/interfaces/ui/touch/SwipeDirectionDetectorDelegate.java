b8b8ef1cc32484686365b9a7313f8418be6a399f
vexonelite <elite@realtime.tw>
2018-10-05 15:21:08 +0800

    add 3 Touch relevant interfaces

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/ui/touch/SwipeDirectionDetectorDelegate.java
@@ -0,0 +1,17 @@
+package tw.realtime.project.rtbaseframework.interfaces.ui.touch;
+
+
+import android.support.annotation.NonNull;
+
+import tw.realtime.project.rtbaseframework.enumerations.SwipeDirection;
+
+/**
+ * Created by vexonelite on 2018/10/02.
+ */
+
+public interface SwipeDirectionDetectorDelegate {
+    /**
+     * the callback of a captured swipe gesture
+     */
+    void onDirectionDetected(@NonNull SwipeDirection direction);
+}
