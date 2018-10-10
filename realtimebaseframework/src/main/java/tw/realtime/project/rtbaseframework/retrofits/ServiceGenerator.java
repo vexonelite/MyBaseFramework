57c90c1a6a4026c93b2a5d9020286810fd230a86
vexonelite <elite@realtime.tw>
2018-10-10 09:55:36 +0800

    revision of ServiceGenerator (Retrofit)

--- realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/retrofits/ServiceGenerator.java
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/retrofits/ServiceGenerator.java
@@ -1,10 +1,9 @@
 package tw.realtime.project.rtbaseframework.retrofits;
 
-import java.util.concurrent.TimeUnit;
+import android.support.annotation.NonNull;
 
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import tw.realtime.project.rtbaseframework.api.commons.OkHttpUtils;
 
 //import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 //import retrofit2.converter.gson.GsonConverterFactory;
@@ -16,100 +15,43 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
  */
 public class ServiceGenerator {
 
-    public static final long CONNECTION_TIME = 60000;
-    public static final long READ_TIMEOUT = 120000;
-    public static final long WRITE_TIMEOUT = 120000;
-
     private volatile static Retrofit instance;
 
     /** Returns singleton class instance */
-    private static Retrofit getInstance(RetrofitParameter parameter) {
+    private static Retrofit getInstance(@NonNull Retrofit.Builder builder) {
         if (instance == null) {
             synchronized (Retrofit.class) {
                 if (instance == null) {
-                    instance = getRetrofitInstance(parameter);
+                    instance = builder.build();
                 }
             }
         }
         return instance;
     }
 
-    private static Retrofit getRetrofitInstance (RetrofitParameter parameter) {
-        Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl(parameter.baseApiUrl)
-                .client(parameter.okBuilder.build());
+    private static Retrofit.Builder getBasicRetrofitBuilder (@NonNull String baseApiUrl,
+                                                             boolean enableHttpLoggingInterceptor) {
+        return new Retrofit.Builder()
+                .baseUrl(baseApiUrl)
+                .client(OkHttpUtils.getInstance(enableHttpLoggingInterceptor));
 
-        if (parameter.enableGsonConver) {
-            //builder.addConverterFactory(GsonConverterFactory.create());
-        }
-
-        if (parameter.enableRxJavaCallAdapter) {
-            builder.addCallAdapterFactory(RxJava2CallAdapterFactory.create());
-        }
-
-//        if (parameter.enableSimpleXmlConverter) {
-//            Strategy strategy = new AnnotationStrategy();
-//            Serializer serializer = new Persister(strategy);
+//        if (enableGsonConver) {
+//            builder.addConverterFactory(GsonConverterFactory.create());
+//        }
+//        if (enableRxJavaCallAdapter) {
+//            builder.addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+//        }
+//        if (enableSimpleXmlConverter) {
+//            final Strategy strategy = new AnnotationStrategy();
+//            final Serializer serializer = new Persister(strategy);
 //            builder.addConverterFactory(SimpleXmlConverterFactory.create(serializer));
 //            //builder.addConverterFactory(XStreamXmlConverterFactory.create(new XStream(new DomDriver())));
 //        }
-
-        return builder.build();
-    }
-
-
-    /** Retrofit 2 parameter */
-    public static class RetrofitParameter {
-        private boolean enableGsonConver;
-        private boolean enableRxJavaCallAdapter;
-        private boolean enableSimpleXmlConverter;
-        private String baseApiUrl;
-        private OkHttpClient.Builder okBuilder;
-
-        /** Enable Gson Converter */
-        public RetrofitParameter setEnableGsonConverFlag (boolean flag) {
-            enableGsonConver = flag;
-            return this;
-        }
-
-        /** Enable RxJava Call Adapter */
-        public RetrofitParameter setEnableRxJavaCallAdapterFlag (boolean flag) {
-            enableRxJavaCallAdapter = flag;
-            return this;
-        }
-
-        /** Enable Simple Xml Converter */
-//        public RetrofitParameter setEnableSimpleXmlConverterFlag (boolean flag) {
-//            enableSimpleXmlConverter = flag;
-//            return this;
-//        }
-
-        /** set common api prefix url */
-        public RetrofitParameter setBaseApiUrl (String url) {
-            baseApiUrl = url;
-            return this;
-        }
-
-        /** set OkHttpClient Builder */
-        public RetrofitParameter setOkHttpClientBuilder (OkHttpClient.Builder builder) {
-            okBuilder = builder;
-            return this;
-        }
-    }
-
-    /** Return a default OkHttpClient builder */
-    public static OkHttpClient.Builder getOkHttpClientBuilder () {
-        return new OkHttpClient.Builder()
-                .connectTimeout(CONNECTION_TIME, TimeUnit.MILLISECONDS)
-                .readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
-                .writeTimeout(WRITE_TIMEOUT, TimeUnit.MILLISECONDS)
-                .followRedirects(true)
-                .followSslRedirects(true);
     }
 
     /** create a Retrofit 2 service representing a asynchronized api task.  */
-    public static <S> S createService(Class<S> serviceClass, RetrofitParameter parameter) {
-        return getInstance(parameter).create(serviceClass);
+    public static <S> S createService(Class<S> serviceClass, @NonNull Retrofit.Builder builder) {
+        return getInstance(builder).create(serviceClass);
     }
 }
 
