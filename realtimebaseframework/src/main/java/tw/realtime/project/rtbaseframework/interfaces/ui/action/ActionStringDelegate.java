c31f661686fef6bc43bcac39fdf15263c369d279
vexonelite <elite@realtime.tw>
2018-10-19 11:00:29 +0800

    add ActionStringDelegate

---
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/interfaces/ui/action/ActionStringDelegate.java
@@ -0,0 +1,9 @@
+package tw.realtime.project.rtbaseframework.interfaces.ui.action;
+
+import androidx.annotation.NonNull;
+
+
+public interface ActionStringDelegate {
+    @NonNull
+    String getAction();
+}
