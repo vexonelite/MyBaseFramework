8bcfef45f235079450937704c96830fb3acc64bb
vexonelite <elite@realtime.tw>
2018-11-19 12:25:17 +0800

    major revision of AsyncApiException

--- realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/api/commons/AsyncApiException.java
+++ realtimebaseframework/src/main/java/tw/realtime/project/rtbaseframework/api/commons/AsyncApiException.java
@@ -6,73 +6,66 @@ import androidx.annotation.NonNull;
  * 執行 Api 過程中會丟出的例外
  * <p>
  * Created by vexonelite on 2017/5/25.
+ * revision on 2018/11/19.
  */
 public final class AsyncApiException extends RuntimeException {
 
-    private final String code;
-    private final String optionCode;
+    private final String exceptionCode;
+    private final String statusCode;
+    private final String jsonResponse;
 
-    /**
-     * 建構子
-     * @param theCode    自訂錯誤碼
-     * @param theMessage 錯誤訊息
-     */
-    public AsyncApiException(@NonNull String theCode, @NonNull String theMessage) {
-        super(theMessage);
-        code = theCode;
-        optionCode = "";
+    public AsyncApiException(@NonNull String message,
+                             @NonNull String exceptionCode,
+                             @NonNull String statusCode,
+                             @NonNull String jsonResponse) {
+        super(message);
+        this.exceptionCode = exceptionCode;
+        this.statusCode = statusCode;
+        this.jsonResponse = jsonResponse;
+    }
+
+    public AsyncApiException(@NonNull String message,
+                             @NonNull Throwable cause,
+                             @NonNull String exceptionCode,
+                             @NonNull String statusCode,
+                             @NonNull String jsonResponse) {
+        super(message, cause);
+        this.exceptionCode = exceptionCode;
+        this.statusCode = statusCode;
+        this.jsonResponse = jsonResponse;
+    }
+
+    public AsyncApiException(@NonNull Throwable cause,
+                             @NonNull String exceptionCode,
+                             @NonNull String statusCode,
+                             @NonNull String jsonResponse) {
+        super(cause);
+        this.exceptionCode = exceptionCode;
+        this.statusCode = statusCode;
+        this.jsonResponse = jsonResponse;
     }
 
     /**
-     * 建構子
-     * @param theCode   自訂錯誤碼
-     * @param throwable 錯誤物件
+     *
      */
-    public AsyncApiException(@NonNull String theCode, @NonNull Throwable throwable) {
-        super(throwable);
-        code = theCode;
-        optionCode = "";
+    @NonNull
+    public String getExceptionCode () {
+        return exceptionCode;
     }
 
     /**
-     * 建構子
-     * @param theCode    自訂錯誤碼
-     * @param theMessage 錯誤訊息
-     * @param optCode    額外輔助碼
-     */
-    public AsyncApiException(@NonNull String theCode, @NonNull String theMessage, @NonNull String optCode) {
-        super(theMessage);
-        code = theCode;
-        optionCode = optCode;
-    }
-
-    /**
-     * 建構子
-     * @param theCode   自訂錯誤碼
-     * @param throwable 錯誤物件
-     * @param optCode   額外輔助碼
-     */
-    public AsyncApiException(@NonNull String theCode, @NonNull Throwable throwable, @NonNull String optCode) {
-        super(throwable);
-        code = theCode;
-        optionCode = optCode;
-    }
-
-    /**
-     * 取得自訂錯誤碼
-     * @return 自訂錯誤碼
+     *
      */
     @NonNull
     public String getStatusCode () {
-        return code;
+        return statusCode;
     }
 
     /**
-     * 取得額外輔助碼
-     * @return 額外輔助碼
+     *
      */
     @NonNull
-    public String getOptionCode () {
-        return optionCode;
+    public String getJsonResponse () {
+        return jsonResponse;
     }
 }
