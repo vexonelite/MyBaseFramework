ad366e6186d32fbcd0acb2c854e8bea173563c66
elite_lin <elite_lin@goglobal.com.tw>
2019-09-25 10:39:17 +0800

    add public final class LiveDataWrapper<T>

---
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/architecture/livedatas/LiveDataWrapper.java
@@ -0,0 +1,31 @@
+package tw.realtime.project.rtbaseframework.architecture.livedatas;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+
+public final class LiveDataWrapper<T> {
+
+//    private final byte[] lock = new byte[0];
+    private final MutableLiveData<T> mutableLiveData = new MutableLiveData<>();
+
+    @MainThread
+    public LiveData<T> retrieveLiveData() {
+        return mutableLiveData;
+    }
+
+    @MainThread
+    public void setItemToLiveData(@Nullable T item) {
+        mutableLiveData.setValue(item);
+    }
+
+    @MainThread
+    public void postItemToLiveData(@Nullable T item) {
+        mutableLiveData.postValue(item);
+//        synchronized (lock) {
+//            mutableLiveData.postValue(item);
+//        }
+    }
+}
