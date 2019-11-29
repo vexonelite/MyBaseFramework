8a4301c5afd7b78d14dfe3dd7807e1c2b5a68eba
elite_lin <elite_lin@goglobal.com.tw>
2019-11-29 17:15:22 +0800

    add public final class ExecutorTermination implements Action

---
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/ExecutorTermination.java
@@ -0,0 +1,23 @@
+package tw.com.goglobal.project.rxjava2;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.ExecutorService;
+
+import io.reactivex.functions.Action;
+
+/**
+ * Implementation of Rx Action interface.
+ * It's mainly to have an ExecutorService shutdown.
+ */
+public final class ExecutorTermination implements Action {
+
+    private ExecutorService mExecutorService;
+
+    public ExecutorTermination(@NonNull ExecutorService executorService) { mExecutorService = executorService; }
+
+    @Override
+    public void run() throws Exception {
+        if (null != mExecutorService) { mExecutorService.shutdown(); }
+    }
+}
