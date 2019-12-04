d155b23f344dee1ce0f3fe68688a678670de5376
elite_lin <elite_lin@goglobal.com.tw>
2019-12-04 13:29:12 +0800

    add lots of methods relevant to Wi-Fi connectivity

--- ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/utils/ConnectivityUtils.java
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/utils/ConnectivityUtils.java
@@ -2,21 +2,31 @@ package tw.realtime.project.rtbaseframework.utils;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.net.NetworkSpecifier;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiNetworkSpecifier;
+import android.net.wifi.WifiNetworkSuggestion;
 import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import tw.realtime.project.rtbaseframework.LogWrapper;
@@ -24,6 +34,37 @@ import tw.realtime.project.rtbaseframework.LogWrapper;
 
 public final class ConnectivityUtils {
 
+    @SuppressLint("MissingPermission")
+    public static int isWifiEnabled(@NonNull Context context) {
+        final WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (null == wifiManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "isWifiEnabled - WifiManager is null!");
+            return -1;
+        }
+        if (wifiManager.isWifiEnabled()) { return 1; }
+        else { return 0; }
+    }
+
+    @SuppressLint("MissingPermission")
+    public static void enableWiFi(@NonNull Activity activity, final int requestCode) {
+        final WifiManager wifiManager = (WifiManager) activity.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (null == wifiManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "enableWiFi - WifiManager is null!");
+            return;
+        }
+        if (wifiManager.isWifiEnabled()) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "enableWiFi - Wifi is Enabled now!");
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            final Intent panelIntent = new Intent(Settings.Panel.ACTION_WIFI);
+            activity.startActivityForResult(panelIntent, requestCode);
+        }
+        else { wifiManager.setWifiEnabled(true); }
+    }
+
+    ///
+    
     @SuppressLint("MissingPermission")
     public static boolean preAndroidMInternetCheck(
             @NonNull ConnectivityManager connectivityManager,
@@ -66,6 +107,7 @@ public final class ConnectivityUtils {
         } else { return preAndroidMInternetCheck(connectivityManager, networkTypeList); }
     }
 
+    ///
 
     @NonNull
     @SuppressLint("MissingPermission")
@@ -108,6 +150,92 @@ public final class ConnectivityUtils {
 
         return "";
     }
+
+    ///
+
+    /**
+     * I have tested to involve the method, but it does not work out at all.
+     * And I always got the status [WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_ADD_DUPLICATE]
+     * <p>
+     * The method can only be involved for the case (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+     */
+    public static int connectToSpecifiedRouterViaWifiNetworkSuggestion(
+            @NonNull Context context, @NonNull String networkSSID, @NonNull String networkPassword) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) { return -1; }
+        final WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (null == wifiManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - WifiManager is null!");
+            return -1;
+        }
+        if (!wifiManager.isWifiEnabled()) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - Wifi is disabled!");
+            return -1;
+        }
+
+//        final WifiNetworkSuggestion suggestion1 = new WifiNetworkSuggestion.Builder()
+//                .setSsid(networkSSID)
+//                .setWpa2Passphrase(networkPassword)
+//                .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+//                .build();
+//
+//        final WifiNetworkSuggestion suggestion2 = new WifiNetworkSuggestion.Builder()
+//                .setSsid(networkSSID)
+//                .setWpa3Passphrase(networkPassword)
+//                .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+//                .build();
+
+        final WifiNetworkSuggestion suggestion = new WifiNetworkSuggestion.Builder()
+                .setSsid(networkSSID)
+                //.setWpa2Passphrase(networkPassword)
+                //.setWpa3Passphrase(networkPassword)
+                .setIsAppInteractionRequired(false) // Optional (Needs location permission)
+                .build();
+
+        final List<WifiNetworkSuggestion> suggestionsList = new ArrayList<>();
+//        suggestionsList.add(suggestion1);
+//        suggestionsList.add(suggestion2);
+        suggestionsList.add(suggestion);
+
+        wifiManager.addNetworkSuggestions(suggestionsList);
+        final int status = wifiManager.addNetworkSuggestions(suggestionsList);
+        switch (status) {
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_INTERNAL!");
+                return 1;
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_INTERNAL:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_INTERNAL!");
+                return 0;
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_APP_DISALLOWED:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_APP_DISALLOWED!");
+                return 0;
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_ADD_DUPLICATE:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_ADD_DUPLICATE!");
+//                final int result = wifiManager.removeNetworkSuggestions(suggestionsList);
+//                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - after remove: "  + result);
+                return 0;
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_ADD_EXCEEDS_MAX_PER_APP:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_ADD_EXCEEDS_MAX_PER_APP!");
+                return 0;
+            case WifiManager.STATUS_NETWORK_SUGGESTIONS_ERROR_REMOVE_INVALID:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_REMOVE_INVALID!");
+                return 0;
+            default:
+                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaWifiNetworkSuggestion - STATUS_NETWORK_SUGGESTIONS_ERROR_REMOVE_INVALID!");
+                return -1;
+        }
+    }
+
+    /**
+     * The method can only be involved for the case (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+     */
+    public static void registerWiFiSuggestionBroadcast(@NonNull Context context, @NonNull BroadcastReceiver receiver) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) { return; }
+        final IntentFilter intentFilter =
+                new IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
+        context.registerReceiver(receiver, intentFilter);
+    }
+
+    ///
 
     /**
      * The method can only be involved for the case (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
@@ -164,38 +292,138 @@ public final class ConnectivityUtils {
         return null;
     }
 
+    ///
+
+    @NonNull
+    @TargetApi(Build.VERSION_CODES.Q)
+    public static WifiNetworkSpecifier getWifiNetworkSpecifier(
+            @NonNull String networkSSID, @NonNull String networkPassword, final boolean doesUseWPA3) {
+        final WifiNetworkSpecifier.Builder builder = new WifiNetworkSpecifier.Builder()
+                .setSsid(networkSSID);
+        if (doesUseWPA3) { builder.setWpa3Passphrase(networkPassword); }
+        else { builder.setWpa2Passphrase(networkPassword); }
+        return builder.build();
+    }
+
+    @NonNull
+    @TargetApi(Build.VERSION_CODES.Q)
+    public static NetworkRequest getNetworkRequest(
+            @NonNull NetworkSpecifier specifier, final int requiredTransportType) {
+        final NetworkRequest.Builder builder = new NetworkRequest.Builder()
+                .addTransportType(requiredTransportType)
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED)
+                .setNetworkSpecifier(specifier);
+        return builder.build();
+    }
+
+    @NonNull
+    @TargetApi(Build.VERSION_CODES.Q)
+    public static NetworkRequest getNetworkRequest(
+            @NonNull NetworkSpecifier specifier,
+            final int requiredTransportType,
+            final boolean doesNeedToExclude,
+            final int excludedTransportType,
+            final boolean internetIsNotRequired) {
+        final NetworkRequest.Builder builder = new NetworkRequest.Builder()
+//                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+//                .removeTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addTransportType(requiredTransportType)
+                .setNetworkSpecifier(specifier);
+        if (doesNeedToExclude) { builder.removeTransportType(excludedTransportType); }
+        if (internetIsNotRequired) { builder.removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET); }
+        return builder.build();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static int connectToSpecifiedRouterViaRequestNetwork(
+            @NonNull Context context,
+            @NonNull NetworkRequest request,
+            @NonNull ConnectivityManager.NetworkCallback callback) {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivityManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaRequestNetwork - ConnectivityManager is null!");
+            return -1;
+        }
+        try {
+            connectivityManager.requestNetwork(request, callback);
+            return 1;
+        }
+        catch (Exception cause) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaRequestNetwork - Error on ConnectivityManager#requestNetwork is null!", cause);
+            return 0;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static int connectivityManagerUnregisterNetworkCallback(
+            @NonNull Context context, @NonNull ConnectivityManager.NetworkCallback callback) {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivityManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaRequestNetwork - ConnectivityManager is null!");
+            return -1;
+        }
+        try {
+            connectivityManager.unregisterNetworkCallback(callback);
+            return 1;
+        }
+        catch (Exception cause) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectToSpecifiedRouterViaRequestNetwork - Error on ConnectivityManager#unregisterNetworkCallback is null!", cause);
+            return 0;
+        }
+    }
+
+    ///
+
     @SuppressLint("MissingPermission")
-    public static void connectivityManagerBindProcessToNetwork(@NonNull Context context, @NonNull Network network) {
+    public static int isNetworkInterestedTransportType(
+            @NonNull Context context, @NonNull Network network, final int transportType) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) { return -1; }
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivityManager) {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "isNetworkInterestedTransportType - ConnectivityManager is null!");
+            return -1;
+        }
+        final NetworkCapabilities connection = connectivityManager.getNetworkCapabilities(network);
+        if (null != connection) {
+//            if (connection.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) { return 1; }
+//            if (connection.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {return 0; }
+            if (connection.hasCapability(transportType)) { return 1; }
+            else {
+                LogWrapper.showLog(Log.ERROR, "ConnectivityManager ktx", "unknown transport type!");
+                return 0;
+            }
+        }
+        else {
+            LogWrapper.showLog(Log.ERROR, "ConnectivityManager ktx", "NetworkCapability is null!");
+            return -1;
+        }
+    }
+
+    ///
+
+    @SuppressLint("MissingPermission")
+    public static void connectivityManagerBindProcessToNetwork(@NonNull Context context, @Nullable Network network) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) { return; }
-        final ConnectivityManager connManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (null == connManager) {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivityManager) {
             LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerBindProcessToNetwork - ConnectivityManager is null!");
             return;
         }
 
-        final NetworkCapabilities connection = connManager.getNetworkCapabilities(network);
-        if (null != connection) {
-            if (connection.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerBindProcessToNetwork - WIFI");
-            }
-            if (connection.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerBindProcessToNetwork - CELLULAR");
-            }
-        }
-
-        try { connManager.bindProcessToNetwork(network); }
+        try { connectivityManager.bindProcessToNetwork(network); }
         catch (Exception cause) { LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "bindProcessToNetwork - ConnectivityManager#bindProcessToNetwork", cause); }
     }
 
-    public static void connectivityManagerClearProcessBinding(@NonNull Context context) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) { return; }
-        final ConnectivityManager connManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (null == connManager) {
-            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerClearProcessBinding - ConnectivityManager is null!");
-            return;
-        }
-        try { connManager.bindProcessToNetwork(null); }
-        catch (Exception cause) { LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerClearProcessBinding - ConnectivityManager#bindProcessToNetwork", cause); }
-    }
+//    public static void connectivityManagerClearProcessBinding(@NonNull Context context) {
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) { return; }
+//        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if (null == connectivityManager) {
+//            LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerClearProcessBinding - ConnectivityManager is null!");
+//            return;
+//        }
+//        try { connectivityManager.bindProcessToNetwork(null); }
+//        catch (Exception cause) { LogWrapper.showLog(Log.ERROR, "ConnectivityUtils", "connectivityManagerClearProcessBinding - ConnectivityManager#bindProcessToNetwork", cause); }
+//    }
+
 }
 
