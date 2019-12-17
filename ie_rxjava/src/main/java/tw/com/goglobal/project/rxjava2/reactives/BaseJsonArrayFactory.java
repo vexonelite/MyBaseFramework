aea68561db195b90c5377fc72d6b2c98cc9ad991
elite_lin <elite_lin@goglobal.com.tw>
2019-12-17 10:00:57 +0800

    add      public abstract class BaseJsonArrayFactory<T> extends AbstractRxTask<T> implements Callable<String>      public abstract class JsonStringToObjectList<T> extends IeBaseConverter<String, List<T>> implements Function<String, List<T>>

---
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/BaseJsonArrayFactory.java
@@ -0,0 +1,46 @@
+package tw.com.goglobal.project.rxjava2.reactives;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RawRes;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import tw.com.goglobal.project.rxjava2.AbstractRxTask;
+import tw.realtime.project.rtbaseframework.utils.CodeUtils;
+
+
+public abstract class BaseJsonArrayFactory<T> extends AbstractRxTask<T> implements Callable<String> {
+
+    private final Context context;
+
+    @RawRes
+    private final int resId;
+
+    public BaseJsonArrayFactory(@NonNull Context context, @RawRes final int resId) {
+        this.context = context;
+        this.resId = resId;
+    }
+
+    @Override
+    public final String call() throws Exception {
+        return CodeUtils.readJsonStringFromResRawFile(context, resId);
+    }
+
+    @Override
+    public void runTask() {
+        rxDisposeIfPossible();
+        setDisposable(
+                getRxSingle()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new ApiDisposableSingleObserver())
+        );
+    }
+
+    protected abstract Single<T> getRxSingle();
+}
---
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/JsonStringToObjectList.java
@@ -0,0 +1,36 @@
+package tw.com.goglobal.project.rxjava2.reactives;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import io.reactivex.functions.Function;
+import tw.com.goglobal.project.httpstuff.MoshiUtil;
+import tw.realtime.project.rtbaseframework.apis.converters.IeBaseConverter;
+import tw.realtime.project.rtbaseframework.apis.errors.ErrorCodes;
+import tw.realtime.project.rtbaseframework.apis.errors.IeRuntimeException;
+
+
+public abstract class JsonStringToObjectList<T>
+        extends IeBaseConverter<String, List<T>> implements Function<String, List<T>> {
+
+    private Class<T> tClass;
+
+    public JsonStringToObjectList(@NonNull Class<T> tClass) { this.tClass = tClass; }
+
+    @NonNull
+    @Override
+    public List<T> apply(@NonNull String jsonString) throws IeRuntimeException {
+        return convertIntoData(jsonString);
+    }
+
+    @NonNull
+    @Override
+    protected List<T> doConversion(@NonNull String input) throws Exception {
+        final Type type = MoshiUtil.getListType(tClass);
+        final List<T> resultList = MoshiUtil.decodeJsonByMoshi(MoshiUtil.newInstanceOfMoshi(), input, type);
+        if (null == resultList) { throw new IeRuntimeException("resultList is null", ErrorCodes.Base.JSON_PARSING_FAILURE); }
+        return resultList;
+    }
+}
