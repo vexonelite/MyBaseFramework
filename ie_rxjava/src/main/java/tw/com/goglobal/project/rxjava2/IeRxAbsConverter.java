e86a7aaf6299b58944569c3f695a7dce448af2b8
elite_lin <elite_lin@goglobal.com.tw>
2019-12-17 09:59:59 +0800

    add      public final class IeSparseArrayHolder<T>      public abstract class IeRxAbsConverter<T, R> extends IeBaseConverter<T, R> implements Function<T, R>

---
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/IeRxAbsConverter.java
@@ -0,0 +1,14 @@
+package tw.com.goglobal.project.rxjava2;
+
+import androidx.annotation.NonNull;
+
+import io.reactivex.functions.Function;
+import tw.realtime.project.rtbaseframework.apis.converters.IeBaseConverter;
+import tw.realtime.project.rtbaseframework.apis.errors.IeRuntimeException;
+
+
+public abstract class IeRxAbsConverter<T, R> extends IeBaseConverter<T, R> implements Function<T, R> {
+    @NonNull
+    @Override
+    public R apply(@NonNull T input) throws IeRuntimeException { return convertIntoData(input); }
+}
