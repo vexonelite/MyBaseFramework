667df4284aa8850db1bc66d38095d0e2e20e8417
elite_lin <elite_lin@goglobal.com.tw>
2020-02-26 08:54:13 +0800

    add     public abstract class IeRxAbsFilter<T> extends IeBaseFilter<T> implements Predicate<T>     public final class GsonJsonStringToObjectConverter2<T> extends IeBaseConverter<String, T> implements Function<String, T>
    revision of error handling ``GsonJsonStringToObjectConverter``

--- ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/GsonJsonStringToObjectConverter2.java
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/GsonJsonStringToObjectConverter2.java
@@ -4,18 +4,20 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 
+import java.lang.reflect.Type;
+
 import io.reactivex.functions.Function;
 import tw.realtime.project.rtbaseframework.apis.converters.IeBaseConverter;
 import tw.realtime.project.rtbaseframework.apis.errors.ErrorCodes;
 import tw.realtime.project.rtbaseframework.apis.errors.IeRuntimeException;
 
 
-public final class GsonJsonStringToObjectConverter<T>
+public final class GsonJsonStringToObjectConverter2<T>
         extends IeBaseConverter<String, T> implements Function<String, T> {
 
-    private Class<T> tClass;
+    private Type type;
 
-    public GsonJsonStringToObjectConverter(@NonNull Class<T> tClass) { this.tClass = tClass; }
+    public GsonJsonStringToObjectConverter2(@NonNull Type type) { this.type = type; }
 
     @NonNull
     @Override
@@ -27,7 +29,7 @@ public final class GsonJsonStringToObjectConverter<T>
     @Override
     protected T doConversion(@NonNull String input) throws IeRuntimeException {
         try {
-            final T result = new Gson().fromJson(input, tClass);
+            final T result = new Gson().fromJson(input, type);
             if (null != result) { return result; }
             else { throw new IeRuntimeException("Gson().fromJson returns a null result!!", ErrorCodes.Base.INTERNAL_CONVERSION_ERROR); }
         }
