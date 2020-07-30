2792f846a4d82dcbab81c7571076e0e62487dc98
elite_lin <elite_lin@goglobal.com.tw>
2020-07-30 10:11:53 +0800

    Wi-Fi relevant - add:
    1. public enum IeNetworkCallbackState {
        LOST,
        UNAVAILABLE,
        AVAILABLE,
        CAPABILITIES_CHANGED,
        LINK_PROPERTIES_CHANGED,
        DEFAULT,
    }
    
    2. public final class IeNetworkStateInfo {}

---
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/networks/wlan/IeNetworkCallbackState.java
@@ -0,0 +1,11 @@
+package tw.realtime.project.rtbaseframework.widgets.networks.wlan;
+
+
+public enum IeNetworkCallbackState {
+    LOST,
+    UNAVAILABLE,
+    AVAILABLE,
+    CAPABILITIES_CHANGED,
+    LINK_PROPERTIES_CHANGED,
+    DEFAULT,
+}
