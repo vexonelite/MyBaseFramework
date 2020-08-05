5f47d0c24c9f348317d412d69e78b063e8c046e5
elite_lin <elite_lin@goglobal.com.tw>
2020-08-05 11:40:26 +0800

    add log

--- ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/AbsRxNetworkStateCallback.java
+++ ie_rxjava/src/main/java/tw/com/goglobal/project/rxjava2/reactives/AbsRxNetworkStateCallback.java
@@ -14,25 +14,36 @@ import tw.realtime.project.rtbaseframework.widgets.networks.wlan.IeNetworkStateI
 public abstract class AbsRxNetworkStateCallback implements IeNetworkDelegate, Consumer<Object> {
     @Override
     public final void accept(@NonNull Object anyObject) throws Exception {
+        LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept: " + anyObject.getClass().getSimpleName());
         if (!(anyObject instanceof IeNetworkStateInfo)) { return; }
         final IeNetworkStateInfo networkStateInfo = (IeNetworkStateInfo) anyObject;
+        LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept: " + anyObject.getClass().getSimpleName());
         switch (networkStateInfo.callbackState) {
             case AVAILABLE: {
+                LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept - AVAILABLE");
                 if (null != networkStateInfo.network ) { this.onAvailable(networkStateInfo.network); }
                 else { LogWrapper.showLog(Log.ERROR, "AbsRxNetworkStateCallback", "accept - AVAILABLE - networkStateInfo.network is null!!"); }
                 break;
             }
             case CAPABILITIES_CHANGED: {
+                LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept - CAPABILITIES_CHANGED");
                 if ( (null != networkStateInfo.network ) && (null != networkStateInfo.networkCapabilities) ) {
                     this.onCapabilitiesChanged(networkStateInfo.network, networkStateInfo.networkCapabilities);
                 }
-                else { LogWrapper.showLog(Log.ERROR, "AbsRxNetworkStateCallback", "accept - CAPABILITIES_CHANGED - " +
-                        "either networkStateInfo.network or networkStateInfo.networkCapabilities is null!!"); }
+                else {
+                    LogWrapper.showLog(Log.ERROR, "AbsRxNetworkStateCallback", "accept - CAPABILITIES_CHANGED - " +
+                            "either networkStateInfo.network or networkStateInfo.networkCapabilities is null!!");
+                }
                 break;
             }
             case LOST: {
+                LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept - LOST");
                 if (null != networkStateInfo.network ) { this.onLost(networkStateInfo.network); }
                 else { LogWrapper.showLog(Log.ERROR, "AbsRxNetworkStateCallback", "accept - LOST - networkStateInfo.network is null!!"); }
+                break;
+            }
+            default: {
+                LogWrapper.showLog(Log.INFO, "AbsRxNetworkStateCallback", "accept - networkStateInfo.callbackState: " + networkStateInfo.callbackState);
                 break;
             }
         }
