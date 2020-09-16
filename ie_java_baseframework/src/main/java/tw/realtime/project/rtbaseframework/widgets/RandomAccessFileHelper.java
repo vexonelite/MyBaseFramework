b6e31cefb98e79f8254fad2f16c04bb1d2dae0be
elite_lin <elite_lin@goglobal.com.tw>
2020-09-16 15:43:45 +0800

    add public final class RandomAccessFileHelper {}

---
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/RandomAccessFileHelper.java
@@ -0,0 +1,85 @@
+package tw.realtime.project.rtbaseframework.widgets;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+
+import tw.realtime.project.rtbaseframework.LogWrapper;
+import tw.realtime.project.rtbaseframework.utils.CodeUtils;
+
+public final class RandomAccessFileHelper {
+
+    private final RandomAccessFile randomAccessFile;
+
+    private byte[] byteArray;
+
+    private String getLogTag() { return this.getClass().getSimpleName(); }
+
+    public RandomAccessFileHelper(@NonNull final RandomAccessFile randomAccessFile) {
+        this.randomAccessFile = randomAccessFile;
+    }
+
+    public RandomAccessFileHelper initializeByteArray(final int numberOfBytes) {
+        this.byteArray = new byte[numberOfBytes];
+        LogWrapper.showLog(Log.INFO, getLogTag(), "initializeByteArray - numberOfBytes: " + numberOfBytes + ", byteArray.length: " + byteArray.length);
+        return this;
+    }
+
+    public RandomAccessFileHelper readBytes(@NonNull final String propertyName) {
+        try {
+            randomAccessFile.read(byteArray);
+            LogWrapper.showLog(Log.INFO, getLogTag(), "readBytes - [" + propertyName + "] FilePointer: " + randomAccessFile.getFilePointer() + ", Hex: " + IeUtils.byteArrayToHexString(byteArray));
+        }
+        catch (Exception cause) {
+            LogWrapper.showLog(Log.ERROR, getLogTag(), "error on readBytes - [" + propertyName + "]", cause.fillInStackTrace());
+        }
+        return this;
+    }
+
+    @NonNull
+    public String convertIntoString(@NonNull final String defaultValue) {
+        if (null != byteArray) {
+            try { return new String(byteArray, StandardCharsets.UTF_8); }
+            catch (Exception cause) {
+                LogWrapper.showLog(Log.ERROR, getLogTag(), "error on convertIntoString", cause.fillInStackTrace());
+                return defaultValue;
+            }
+        }
+        else {
+            LogWrapper.showLog(Log.ERROR, getLogTag(), "convertIntoString - byteArray is nul!!");
+            return defaultValue;
+        }
+    }
+
+    public int convertIntoInteger(@NonNull final ByteOrder byteOrder, final int defaultValue) {
+        if (null != byteArray) {
+            try { return CodeUtils.byteArrayToIntegerWithOrder(byteArray, byteOrder, defaultValue); }
+            catch (Exception cause) {
+                LogWrapper.showLog(Log.ERROR, getLogTag(), "error on convertIntoInteger", cause.fillInStackTrace());
+                return defaultValue;
+            }
+        }
+        else {
+            LogWrapper.showLog(Log.ERROR, getLogTag(), "convertIntoInteger - byteArray is nul!!");
+            return defaultValue;
+        }
+    }
+
+    public long convertIntoLong(@NonNull final ByteOrder byteOrder, final long defaultValue) {
+        if (null != byteArray) {
+            try { return CodeUtils.byteArrayToLongWithOrder(byteArray, byteOrder, defaultValue); }
+            catch (Exception cause) {
+                LogWrapper.showLog(Log.ERROR, getLogTag(), "error on convertIntoLong", cause.fillInStackTrace());
+                return defaultValue;
+            }
+        }
+        else {
+            LogWrapper.showLog(Log.ERROR, getLogTag(), "convertIntoLong - byteArray is nul!!");
+            return defaultValue;
+        }
+    }
+}
