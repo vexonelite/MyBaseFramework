cdc95cd94fc490f2fce61cd0dd05f32d1e9ca1f6
elite_lin <elite_lin@goglobal.com.tw>
2020-10-14 17:43:10 +0800

    support     public final OkHttpConfigure configure;

--- ie_java_httpstuff/src/main/java/tw/com/goglobal/project/httpstuff/callables/SimpleHttpGetCallable.java
+++ ie_java_httpstuff/src/main/java/tw/com/goglobal/project/httpstuff/callables/SimpleHttpGetCallable.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Callable;
 
 import okhttp3.Call;
 import okhttp3.Response;
+import tw.com.goglobal.project.httpstuff.OkHttpConfigure;
 import tw.com.goglobal.project.httpstuff.OkHttpUtil;
 import tw.realtime.project.rtbaseframework.apis.errors.ErrorCodes;
 import tw.realtime.project.rtbaseframework.apis.errors.IeHttpException;
@@ -15,16 +16,23 @@ import tw.realtime.project.rtbaseframework.apis.errors.IeHttpException;
 public final class SimpleHttpGetCallable implements Callable<Response> {
 
     public final String url;
+    public final OkHttpConfigure configure;
 
-    public SimpleHttpGetCallable(@NonNull String url) {
+    public SimpleHttpGetCallable(@NonNull final String url) {
         this.url = url;
+        this.configure = new OkHttpConfigure();
+    }
+
+    public SimpleHttpGetCallable(@NonNull final String url, @NonNull final OkHttpConfigure configure) {
+        this.url = url;
+        this.configure = configure;
     }
 
     @Override
     public Response call() throws Exception {
         try {
             final Call requestCall = new OkHttpUtil()
-                    .generateHttpGetRequestCall(url, true, false);
+                    .generateHttpGetRequestCall(url, true, false, configure);
             return requestCall.execute();
         }
         catch (Exception cause) {
