f8a4efbba130ac3e1e463d55ea25f2cb24bf73c4
elite_lin <elite_lin@goglobal.com.tw>
2020-10-27 16:21:57 +0800

    add      public final class AppUncaughtExceptionHandler<T> implements Thread.UncaughtExceptionHandler {}

---
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/app/AppUncaughtExceptionHandler.java
@@ -0,0 +1,37 @@
+package tw.realtime.project.rtbaseframework.app;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+
+public final class AppUncaughtExceptionHandler<T> implements Thread.UncaughtExceptionHandler {
+
+    private final Activity activity;
+    private final Class<T> activityClass;
+
+    public AppUncaughtExceptionHandler(@NonNull final Activity activity,
+                                       @NonNull final Class<T> activityClass) {
+        this.activity = activity;
+        this.activityClass = activityClass;
+    }
+
+    @Override
+    public void uncaughtException(@NonNull Thread thread, @NonNull Throwable ex) {
+        final Intent intent = new Intent(activity, activityClass);
+        intent.putExtra("crash", true);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(
+                activity.getBaseContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        final AlarmManager mgr = (AlarmManager) activity.getBaseContext().getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, pendingIntent);
+        activity.finish();
+        System.exit(2);
+    }
+}
