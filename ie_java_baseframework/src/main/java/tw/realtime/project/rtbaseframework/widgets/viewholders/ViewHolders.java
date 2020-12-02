26dc0cdb39f8989783d4b0e1ab1b3e9bfaa6df80
elite_lin <elite_lin@goglobal.com.tw>
2020-12-02 10:55:25 +0800

    add 1. class IeSingleTextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {} 2. xml layout: layout\base_adapter_single_text_viewholder.xml

--- ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/viewholders/ViewHolders.java
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/viewholders/ViewHolders.java
@@ -1,8 +1,12 @@
 package tw.realtime.project.rtbaseframework.widgets.viewholders;
 
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import tw.realtime.project.rtbaseframework.delegates.ui.view.DescriptionDelegate;
 
 /**
  * Default ViewHolder for the default ViewType
@@ -26,5 +30,17 @@ public final class ViewHolders {
     public static final class MatchParentUnavailable extends RecyclerView.ViewHolder {
         public MatchParentUnavailable(View itemView) { super(itemView); }
     }
+
+    // [start] added in 2020/12/02
+    public static final class IeSingleText extends RecyclerView.ViewHolder {
+        public IeSingleText(View itemView) { super(itemView); }
+
+        public void onBind(@NonNull final DescriptionDelegate delegate, final int position) {
+            if (itemView instanceof TextView) {
+                ((TextView) itemView).setText(delegate.theDescription());
+            }
+        }
+    }
+    // [end] added in 2020/12/02
 }
 
