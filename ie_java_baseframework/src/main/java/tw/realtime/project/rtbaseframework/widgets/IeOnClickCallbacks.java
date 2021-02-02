106b56467ab86c96444feeeda18be2ed62075cd8
elite_lin <elite_lin@goglobal.com.tw>
2021-02-02 10:30:59 +0800

    add     public final class DoNothing implements View.OnClickListener {}     public final class ViewVisibility implements View.OnClickListener {}

---
+++ ie_java_baseframework/src/main/java/tw/realtime/project/rtbaseframework/widgets/IeOnClickCallbacks.java
@@ -0,0 +1,28 @@
+package tw.realtime.project.rtbaseframework.widgets;
+
+import android.view.View;
+
+public final class IeOnClickCallbacks {
+
+    public final class DoNothing implements View.OnClickListener {
+        @Override
+        public void onClick(View view) { }
+    }
+
+
+    public final class ViewVisibility implements View.OnClickListener {
+
+        private final int visibility;
+
+        public ViewVisibility(final int visibility) { this.visibility = visibility; }
+
+        @Override
+        public void onClick(View view) {
+            if ((visibility == View.VISIBLE) || (visibility == View.INVISIBLE) || (visibility == View.GONE) ) {
+                view.setVisibility(visibility);
+            }
+        }
+    }
+
+
+}
